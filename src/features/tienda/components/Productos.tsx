6b7e5a58a0216506fd8e8b79de72f6ac13d78d2c
Milan Guzman <milanguzman@Milans-MacBook-Air.local>
2026-04-10 13:29:24 -0600

    Updated tienda

--- src/features/tienda/components/Productos.tsx
+++ src/features/tienda/components/Productos.tsx
@@ -2,12 +2,8 @@ import { useState, useEffect } from "react";
 import { createClient } from "@supabase/supabase-js";
 import ProductoCard from "./ProductoCard";
 
-const supabaseUrl =
-  import.meta.env.VITE_SUPABASE_URL2 ?? import.meta.env.VITE_SUPABASE_URL ?? "";
-const supabaseKey =
-  import.meta.env.VITE_SUPABASE_ANON_KEY2 ??
-  import.meta.env.VITE_SUPABASE_ANON_KEY ??
-  "";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL2;
+const supabaseKey =import.meta.env.VITE_SUPABASE_ANON_KEY2;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
